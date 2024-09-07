c54c7ef8b69f6c653f93a7244ac836a85cfbcc72
dplaff <BOGUS@EMAIL>
2024-09-06 21:01:49 -0400

    Task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
-    return (
+    return <div>(
+        <Container>
         <div className="App">
+            <Row>
+                <Col>
+                <div style ={ {border: '4px solid red', padding: '4px'}}>
+                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.costacruises.com%2Fports%2Fsantorini.html&psig=AOvVaw0DKp1dO7JaheCn_A-L1wm6&ust=1725753332888000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNC63aXCr4gDFQAAAAAdAAAAABAE" alt="Santorini, Greece, I travelled here this summer" />
+                </div>
+                </Col>
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Donald
+                UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1> This is my header</h1>
+            
+            <p>My Top 5 Favorite Movies</p>
+            <ol>
+                <li> 1917</li>
+                <li> Interstellar</li>
+                <li> The Green Mile</li>
+                <li> Inception</li>
+                <li> Taxi Driver</li>
+            </ol>
+            </Row>
+         <div style={ {border: '1px solid blue', padding: '4px'} }>
+        this will be surrounded by a border and padding.
+        <div>
+        <Button onClick={ () => { console.log("Hello World!") } }>Log Hello World</Button>
+    </div>
+    </div>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
         </div>
     );
+    
+    </Container>
+    </div>
 }
+
 
 export default App;
